9ffd7e789d1eb042468d0c8a7c824591fd43b0d6
1LightHouse1 <vini_nascimento1@outlook.com>
2023-09-21 22:40:58 -0300

    agora o tema fica salvo dando f5 e quando fecha

--- Atividade5/script.js
+++ Atividade5/script.js
@@ -1,8 +1,15 @@
 let qtdVisita = localStorage.getItem('acessos');
+let tema = localStorage.getItem('darkmmode');
 let texto = "Acessos: ";
 const button = document.getElementById('mudartema');
 const body = document.body;
 const telaCentral = document.getElementById('tela-central');
+
+let savetheme = localStorage.getItem('theme');
+
+if (savetheme) {
+     body.classList.add(savetheme);
+}
 
 if(qtdVisita === undefined || qtdVisita === "NaN"){
     qtdVisita = 0;
@@ -18,9 +25,11 @@ button.addEventListener('click', () =>{
         body.classList.remove('darkmode');
         telaCentral.classList.remove('darkmode');
         button.classList.remove('darkmode');
+        localStorage.setItem('theme', '');
     } else {
         body.classList.add('darkmode');
         telaCentral.classList.add('darkmode');
         button.classList.add('darkmode');
+        localStorage.setItem('theme', 'darkmode');
     }
 });
