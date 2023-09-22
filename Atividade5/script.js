bf72ffeb8d4b885cded8564d9916ea04c16fbf8b
1LightHouse1 <vini_nascimento1@outlook.com>
2023-09-21 22:00:44 -0300

    atividade 5 finalizada

--- Atividade5/script.js
+++ Atividade5/script.js
@@ -1,4 +1,8 @@
 let qtdVisita = localStorage.getItem('acessos');
+let texto = "Acessos: ";
+const button = document.getElementById('mudartema');
+const body = document.body;
+const telaCentral = document.getElementById('tela-central');
 
 if(qtdVisita === undefined || qtdVisita === "NaN"){
     qtdVisita = 0;
@@ -7,3 +11,16 @@ if(qtdVisita === undefined || qtdVisita === "NaN"){
 qtdVisita++;
 localStorage.setItem('acessos', qtdVisita);
 
+document.getElementById('acessos').innerHTML =  texto + qtdVisita;
+
+button.addEventListener('click', () =>{
+    if(body.classList.contains('darkmode')){
+        body.classList.remove('darkmode');
+        telaCentral.classList.remove('darkmode');
+        button.classList.remove('darkmode');
+    } else {
+        body.classList.add('darkmode');
+        telaCentral.classList.add('darkmode');
+        button.classList.add('darkmode');
+    }
+});
